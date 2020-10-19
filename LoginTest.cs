e29b711f516ec40d8962d426dc30b33698f24727
Andreea <andreea.draniceanu@gmail.com>
2020-10-19 19:24:41 +0300

    Add project files.

---
+++ LoginTest.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using TestProject.Common.Enums;
+using TestProject.SDK;
+using TestProject.SDK.Interfaces;
+using TestProject.SDK.Reporters;
+using TestProject.SDK.Tests;
+using TestProject.SDK.Tests.Helpers;
+using TestProject_POM.Pages;
+
+namespace TestProject_POM
+{
+    public class LogInTest : IWebTest
+    {
+
+        ExecutionResult IWebTest.Execute(WebTestHelper helper)
+        {
+            var driver = helper.Driver;
+            TestReporter report = helper.Reporter;
+            HomePage homePage = new HomePage(driver);
+
+            driver.Navigate().GoToUrl("https://example.testproject.io/web/");
+
+            homePage.Login();
+            helper.Reporter.Step("Logged in the app", "The login is unsuccessful", "The login is successful",
+                homePage.IsLoginSuccessful(), TakeScreenshotConditionType.Always);
+
+            if (homePage.IsLoginSuccessful())
+            {
+                return ExecutionResult.Passed;
+            }
+            return ExecutionResult.Failed;
+
+
+        }
+    }
+}
---
+++ Pages/HomePage.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.SDK.Tests.Helpers;
+
+namespace TestProject_POM.Pages
+{
+    class HomePage
+    {
+        private IWebDriver driver;
+
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+        private IWebElement Name => driver.FindElement(By.CssSelector("#name"));
+        private IWebElement Password => driver.FindElement(By.CssSelector("#password"));
+        private IWebElement LoginButton => driver.FindElement(By.CssSelector("#login"));
+        private IWebElement LogoutButton => driver.FindElement(By.CssSelector("#logout"));
+        public void Login()
+        {
+            Name.SendKeys("John Smith");
+            Password.SendKeys("12345");
+            LoginButton.Click();
+        }
+
+        public bool IsLoginSuccessful()
+        {
+            return LogoutButton.Displayed;
+        }
+    }
+}
